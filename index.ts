18d64503a297c3f342a17a78f8aa2cd384d4dfe0
Richard Wang <richardwang301@gmail.com>
2021-08-11 00:21:52 +1000

    form and ui changes

--- index.ts
+++ index.ts
@@ -23,15 +23,75 @@ const drawArray = (arr: number[]): void => {
     }
 };
 
-// const arr = generateArray(5);
-const arr = [52, 26, 80, 46, 96];
-console.log(arr);
-drawArray(arr);
-const sort = new Sort(100);
-// sort.BubbleSort();
-// sort.InsertionSort();
-sort.SelectionSort();
-// sort.MergeSort(0, 99);
+const clearAnimation = () => {
+    const animation = document.getElementById("animation");
+    if (animation) animation.innerHTML = "";
+};
+
+const randomButton = document.getElementById("randomButton");
+randomButton?.addEventListener("click", () => {
+    const arraySize = parseInt(
+        (document.getElementById("size-menu") as HTMLInputElement).value
+    );
+    if (isNaN(arraySize)) return;
+    clearAnimation();
+    console.log(arraySize);
+    drawArray(generateArray(arraySize));
+});
+
+const sortButton = document.getElementById("sortButton");
+sortButton?.addEventListener("click", () => {
+    const sortType = parseInt(
+        (document.getElementById("sort-menu") as HTMLInputElement).value
+    );
+    const arraySize = parseInt(
+        (document.getElementById("size-menu") as HTMLInputElement).value
+    );
+    const speed = parseFloat(
+        (document.getElementById("speed-menu") as HTMLInputElement).value
+    );
+    if (isNaN(sortType) || isNaN(arraySize) || isNaN(speed)) {
+        alert("Please fill all options");
+        return;
+    }
+    console.log(speed);
+    clearAnimation();
+    drawArray(generateArray(arraySize));
+    const sort = new Sort(100 / speed);
+    switch (sortType) {
+        case 0:
+            sort.BubbleSort();
+            break;
+
+        case 1:
+            sort.SelectionSort();
+            break;
+        case 2:
+            sort.InsertionSort();
+            break;
+        case 3:
+            sort.MergeSort(0, arraySize - 1);
+            break;
+        case 4:
+            sort.QuickSort([
+                ...(document.querySelectorAll(
+                    ".bar"
+                ) as NodeListOf<HTMLElement>),
+            ]);
+            break;
+        default:
+            break;
+    }
+});
+
+// const arr = generateArray(50);
+// console.log(arr);
+// drawArray(arr);
+// const sort = new Sort(100);
+// // sort.BubbleSort();
+// // sort.InsertionSort();
+// // sort.SelectionSort();
+// // sort.MergeSort(0, 99);
 // sort.QuickSort([
 //     ...(document.querySelectorAll(".bar") as NodeListOf<HTMLElement>),
 // ]);
